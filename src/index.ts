df62024b6385edcac89ecd1d3c7cb936623ca555
Octavio Sifuentes <tudaeva@gmail.com>
2024-10-29 18:19:12 -0700

    added knex with config and pg driver

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
-  res.json({ hello: 'world' });
+  res.json({ hello: 'vatos' });
 });
 
 const port = Number(process.env.PORT || 8080);
