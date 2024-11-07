515f1a9b2fdf992d70c6e24f74a123302e0770a5
Octavio Sifuentes <tudaeva@gmail.com>
2024-11-06 18:32:00 -0800

    change migration

--- migrations/20241030011812_init-db.ts
+++ migrations/20241030011812_init-db.ts
@@ -1,14 +1,14 @@
-import type { Knex } from 'knex';
+import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('posts', (table) => {
     table.specificType('id', 'CHAR(16)').primary();
-    table.string('slug').notNullable().unique();
-    table.string('title').notNullable();
+    table.string('slug', 60).notNullable().unique();
+    table.string('title', 80).notNullable();
     table.text('content');
-    table.timestamp('publishedAt').index();
-    table.timestamp('createdAt').defaultTo(knex.fn.now()).notNullable();
-    table.timestamp('updatedAt').defaultTo(knex.fn.now()).notNullable();
+    table.timestamp('publishedAt');
+    table.timestamp('createdAt').notNullable().defaultTo(knex.fn.now());
+    table.timestamp('updatedAt').notNullable();
   });
 }
 
